dfd20e120959703c4c52e48534a17fe56a926296
vsmiljana <smiljana.vojvodic@gmail.com>
2020-08-03 19:49:19 +0200

    login and registration finished: US-1, US-2

--- mrsisa_SW6_2017/src/main/java/mrsisa_clinical_center/mrsisa_SW6_2017/controller/PatientController.java
+++ mrsisa_SW6_2017/src/main/java/mrsisa_clinical_center/mrsisa_SW6_2017/controller/PatientController.java
@@ -1,6 +1,10 @@
 package mrsisa_clinical_center.mrsisa_SW6_2017.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 import mrsisa_clinical_center.mrsisa_SW6_2017.dto.LoginUserDto;
 import mrsisa_clinical_center.mrsisa_SW6_2017.dto.RegisterUserDto;
 import mrsisa_clinical_center.mrsisa_SW6_2017.model.Patient;
+import mrsisa_clinical_center.mrsisa_SW6_2017.model.Person;
 import mrsisa_clinical_center.mrsisa_SW6_2017.service.PatientService;
 
 @RestController
 @RequestMapping("/usr")
 public class PatientController {
+	
+	@Autowired
+	private HttpSession session;
 	
 	private PatientService patientService;
 	
@@ -28,11 +36,17 @@ public class PatientController {
 		if (patient == null) {
 			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid email and password combination.");
 		}
+		session.setAttribute("currentUser", patient);
 	}
 	
 	
 	@PostMapping("/register")
 	public void register(@RequestBody RegisterUserDto userDto) throws Exception {
+		
+		// ovo mi treba u setup registration.html a ne ovde jer ovde je vec svasta nesto unio u formu omg
+		if (session.getAttribute("currentUser") != null) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User already loged in!");
+		}
 		
 		Patient patient = patientService.findOneByEmailOrSocialSecurityNumber(userDto.getEmail(), userDto.getSsn());
 		if (patient != null) {
@@ -43,7 +57,22 @@ public class PatientController {
 				userDto.getPhoneNumber(), userDto.getAddress(), userDto.getCity(), userDto.getCountry(), userDto.getSsn());
 		
 		patientService.registerPatient(patient);
+		session.setAttribute("currentUser", patient);
 		
+	}
+	
+	@GetMapping("/checkIfLogged")
+	public LoginUserDto checkIfLogged() {
+		if (session.getAttribute("currentUser") == null) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not logged in!");
+		}
+		
+		Patient p = (Patient) session.getAttribute("currentUser");
+		System.out.println(p.getFirstName());
+		System.out.println("da da ulogovan je");
+		//return new ResponseEntity<>(session.getAttribute("currentUser"), HttpStatus.OK); 
+		//return "p";
+		return new LoginUserDto(p.getEmail(), "nebitno");
 	}
 	
 	//login(LoginUserDto )
---
+++ mrsisa_SW6_2017/src/main/resources/static/js/utils.js
@@ -0,0 +1,46 @@
+function checkRegLoad(){
+	
+	$.ajax({
+		url: "/usr/checkIfLogged",
+		type: "GET",
+		contentType: "application/json",
+		//dataType: "json",
+		
+		error: function (response) {
+			$("body").show();
+		},
+		success : function (data) {
+			
+			window.location.replace("/");
+		}
+		
+	}); 
+	
+}
+
+function checkIfLogged(){
+	
+	$.ajax({
+		url: "/usr/checkIfLogged",
+		type: "GET",
+		contentType: "application/json",
+		//dataType: "json",
+		
+		error: function (response) {
+			console.log("upo u error");
+			window.location.replace("/");
+		},
+		success : function (data) {
+			console.log("success");
+			//d = JSON.parse(data.responseText);
+			console.log(data.email);
+			console.log("success moze aj");
+			//alert("ima");
+			$("body").show();
+			console.log("cao " + data.email);
+		}
+		
+	}); 
+	
+	
+}
