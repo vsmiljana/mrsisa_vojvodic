7b5b9e42773d3916aaa653ebf88855820128dd56
vsmiljana <smiljana.vojvodic@gmail.com>
2020-08-29 18:16:33 +0200

    deleting unnecessary files

--- mrsisa_SW6_2017/src/main/java/mrsisa_clinical_center/mrsisa_SW6_2017/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package mrsisa_clinical_center.mrsisa_SW6_2017.service;
-
-import mrsisa_clinical_center.mrsisa_SW6_2017.model.Person;
-
-public interface PersonService {
-	
-	public void addPerson(Person person);
-
-}
